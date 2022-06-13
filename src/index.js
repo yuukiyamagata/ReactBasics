feb6fc8ba959dd89e5e771e720f2182f71201691
yuki yamagata <yshanxing4@gmail.com>
2022-06-14 00:31:34 +0900

    fix: index.jsを修正

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import ReactDOM  from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+const App = () => {
+  return (
+    <>
+      <App />
+    </>
+  );
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// 第一引数 関数コンポーネント
+// 第二引数 どこに反映するか
